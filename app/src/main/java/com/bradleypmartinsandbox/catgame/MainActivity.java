68dbb8d47d4e88515eeff3235ef477ef15c6bd2f
Brad Martin <brma7253@colorado.edu>
2019-01-13 17:50:48 -0700

    added RNG to select winning card idx

--- app/src/main/java/com/bradleypmartinsandbox/catgame/MainActivity.java
+++ app/src/main/java/com/bradleypmartinsandbox/catgame/MainActivity.java
@@ -3,34 +3,65 @@ package com.bradleypmartinsandbox.catgame;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button shuffleButton;
     TextView meowText;
-    ArrayList cards;
+    ArrayList<ImageView> cards;
+    Random randomGenerator;
+    Integer correctGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        shuffleButton = (Button)findViewById(R.id.shuffleButton);
-        meowText = (TextView)findViewById(R.id.meowText);
         cards = new ArrayList<ImageView>();
+        randomGenerator = new Random();
 
-        ImageView card1 = (ImageView)findViewById(R.id.card1);
+        meowText = findViewById(R.id.meowText);
+
+        ImageView card1 = findViewById(R.id.card1);
         cards.add(card1);
 
-        ImageView card2 = (ImageView)findViewById(R.id.card2);
+        ImageView card2 = findViewById(R.id.card2);
         cards.add(card2);
 
-        ImageView card3 = (ImageView)findViewById(R.id.card3);
+        ImageView card3 = findViewById(R.id.card3);
         cards.add(card3);
+
+        shuffleButton = findViewById(R.id.shuffleButton);
+        shuffleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int idx=0; idx<3; idx++) {
+                    ImageView card = cards.get(idx);
+                    card.setImageResource(R.drawable.card);
+                }
+
+                correctGuess = randomGenerator.nextInt(3);
+                meowText.setText("Random Shuffle #: " + correctGuess);
+            }
+        });
+
+        ImageView card;
+        for (int idx=0; idx<3; idx++) {
+            card = cards.get(idx);
+            card.setTag(idx);
+            card.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
     }
 }
